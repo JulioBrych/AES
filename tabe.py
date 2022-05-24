6ab0238ba969d13c185e9349c682484dbb21bf77
Julio Brych <julio_brych15@hotmail.com>
2022-05-23 21:48:12 -0300

    teste

--- tabe.py
+++ tabe.py
@@ -1,4 +1,4 @@
 from AES import *
 
 classeAES = Aes(r"C:\Users\gfibr\OneDrive\Documentos\criptografar\teste2.txt",r"C:\Users\gfibr\OneDrive\Documentos\criptografar\criptografado.txt")
-
+print("Oba")
